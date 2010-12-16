9ac0adac6f84911d7e8ee65d7f8403e1e0ee2590
Blagovest Dachev <blago@dachev.com>
2010-12-15 22:08:51 -0600

    Fixed wikidrill form to disable the form when one of the fields is empty

--- apps/wikidrill/public/js/main.js
+++ apps/wikidrill/public/js/main.js
@@ -86,6 +86,7 @@ $(function() {
         
             if (value == '') {
                 $this.addClass('invalid');
+                disableSubmit();
                 invalidCount++;
             }
             else {
