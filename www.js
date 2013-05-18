cc8d89b33b7d90d371a3937b43a29195b47b005f
Blagovest Dachev <blago@dachev.com>
2013-05-18 15:39:57 -0500

    Node 0.10 compatibility

--- www.js
+++ www.js
@@ -1,155 +1,162 @@
 #!/usr/bin/env node
 
-var Fs   = require('fs')
-var Path = require('path');
+var fs   = require('fs')
+var path = require('path');
+var _    = require('underscore');
 var apps = {};
 
-function AppLoader(server, bayeux, docroot) {
-    var dirs = Fs.readdirSync(docroot);
+function AppLoader(rest, pubsub, docroot) {
+  var dirs = fs.readdirSync(docroot);
+  
+  // load app.js files
+  _.each(dirs, function(val, key) {
+    var appPath = path.join(docroot, val);
+    var appStat = fs.statSync(appPath);
     
-    // load app.js files
-    dirs.forEach(function(name, idx) {
-        var path  = Path.join(docroot, name);
-        var stats = Fs.statSync(path);
-        
-        if (stats.isDirectory() == false) {
-            return;
-        }
-        
-        var file = path + '/app.js';
-        var app  = require(file);
-        
-        Fs.watchFile(file, function(curr, prev) {
-            process.exit(0);
-        });
-        
-        apps[app.name] = app;
+    if (appStat.isDirectory() == false) {
+      return;
+    }
+    
+    var file = path.join(appPath, 'app.js');
+    var app  = require(file);
+    
+    fs.watchFile(file, function(curr, prev) {
+      process.exit(0);
     });
     
-    Object.keys(apps).forEach(function(name, idx) {
-        var app = apps[name];
-        
-        app.init(server, bayeux);
-        
-        server.use('/'+name, app.rest);
-    });
+    apps[app.name] = app;
+  });
+  
+  _.chain(apps).keys().each(function(val, key) {
+    var app = apps[val];
     
-    var self = this;
-    process.nextTick(function() {
-        self.emit('done');
-    });
+    app.init(rest, pubsub);
+    
+    rest.use('/'+val, app.rest);
+  });
+  
+  var self = this;
+  process.nextTick(function() {
+    self.emit('done');
+  });
 }
 AppLoader.prototype = new process.EventEmitter();
 
-function makeOptions(argv) {
-    if (!argv.e) {
-        showBanner();
-        console.log('No environment specified')
-        process.exit(1);
-    }
-    
-    return {env:argv.e};
-}
-
-function showBanner() {
-    var command = Path.basename(__filename),
-        banner  = 'Usage: ' + command + ' -e ENVIRONMENT';
-
-    console.log(banner);
-    console.log(new Array(banner.length+1).join('-'))
-}
-
 function init() {
-    var Express = require('express');
-    var Ejs     = require('ejs');
-    var Faye    = require('faye');
-    var argv    = require('optimist').argv;
-    var opts    = makeOptions(argv);
-    var config  = require('./config')[opts.env]||{};
-    
-    // add a cron job to start the server on reboot
-    require('crontab').load(cronLoaded);
-    function cronLoaded(err, tab) {
-        if (err) { console.log(err); process.exit(1); }
-    
-        var uuid          = '74d967a0-120b-11e0-ac64-0800200c9a66'
-            ,nodePath     = process.execPath.split('/').slice(0, -1).join('/')
-            ,exptCommand  = 'export PATH=' + nodePath + ':$PATH'
-            ,options      = ' -e ' + opts.env
-            ,wwwCommand   = __filename + options
-            ,forevCommand = 'forever'
-            ,sysCommand   = exptCommand + ' && ' + forevCommand + ' start ' + wwwCommand;
-        
-        tab.remove(tab.findComment(uuid));
-    
-        var item = tab.create(sysCommand, uuid);
-        item.everyReboot();
-        tab.save();
-    };
-    
-    var server = Express.createServer();
-    server.set('env', opts.env);
-    
-    // set root templating
-    server.set('views', __dirname + '/views');
-    server.set('view engine', 'html');
-    server.register('.html', Ejs);
-    
-    // add middleware declared in config file
-    var middleware = config.web.middleware||[];
-    for (var i = 0; i < middleware.length; i++) {
-        server.use(middleware[i]);
+  var express = require('express');
+  var faye    = require('faye');
+  var utml    = require('utml');
+  var program = require('commander'); parseArguments(program);
+  var config  = require('./config')[program.environment];
+  
+  // add a cron job to start the server on reboot
+  require('crontab').load(function cronLoaded(err, tab) {
+    if (err) {
+      console.error(err.message.red);
+      process.exit(1);
     }
-    
-    // set root public folder
-    if (opts.env == 'production') {
-        var oneYear = 31557600000;
-        server.use(Express.static(__dirname + '/public', {maxAge:oneYear}));
-    }
-    else {
-        server.use(Express.static(__dirname + '/public'));
-    }
-    
-    var bayeux = new Faye.NodeAdapter({
-        mount   : '/faye',
-        timeout : 45
+
+    var uuid         = '74d967a0-120b-11e0-ac64-0800200c9a66';
+    var nodePath     = process.execPath.split('/').slice(0, -1).join('/');
+    var exptCommand  = 'export PATH=' + nodePath + ':$PATH';
+    var options      = ' -e ' + program.environment;
+    var wwwCommand   = __filename + options;
+    var forevCommand = path.join(__dirname, 'node_modules', 'forever', 'bin', 'forever');
+    var sysCommand   = exptCommand + ' && ' + forevCommand + ' start ' + wwwCommand;
+
+    tab.remove(tab.findComment(uuid));
+
+    var item = tab.create(sysCommand, uuid);
+    item.everyReboot();
+    tab.save();
+  });
+
+  var rest = express();
+  rest.set('env', program.environment);
+  rest.set('port', config.web.port);
+  
+  // set root templating
+  rest.set('views', __dirname + '/views');
+  rest.set('view engine', 'html');
+  rest.engine('html', utml.__express);
+  
+  // add middleware declared in config file
+  var middleware = config.web.middleware||[];
+  for (var i = 0; i < middleware.length; i++) {
+    rest.use(middleware[i]);
+  }
+  
+  // set root public folder
+  if (program.environment == 'production') {
+    var oneYear = 31557600000;
+    rest.use(express.static(__dirname + '/public', {maxAge:oneYear}));
+  }
+  else {
+    rest.use(express.static(__dirname + '/public'));
+  }
+  
+  var pubsub = new faye.NodeAdapter({
+    mount   : '/faye',
+    timeout : 45
+  });
+  
+  var loader = new AppLoader(rest, pubsub, config.web.docroot);
+  loader.on('done', function() {
+    rest.get('/', function(req, res, next) {
+      res.render('index', {
+        locals : {
+          request : req,
+          apps    : apps
+        }
+      });
     });
-    bayeux.attach(server);
     
-    var loader = new AppLoader(server, bayeux, config.web.docroot);
-    loader.on('done', function() {
-        server.get('/', function(req, res, next) {
-            res.render(__dirname + '/views/index', {
-                layout : __dirname + '/views/layout',
-                locals : {request: req, apps:apps}
-            });
-        });
-        
-        server.use(function(req, res) {
-            res.statusCode = 404;
-            res.render(__dirname + '/views/404', {
-                layout  : __dirname + '/views/layout',
-                request : req
-            });
-        });
-        
-        server.error(function(err, req, res) {
-            console.dir(err);
-            
-            res.statusCode = 500;
-            res.render(__dirname + '/views/500', {
-                layout  : __dirname + '/views/layout',
-                request : req,
-                msg     : 'Sorry an error has occurred.'
-            });
-        });
-        
-        server.listen(config.web.port);
+    rest.use(function(req, res) {
+      res.statusCode = 404;
+      res.render(__dirname + '/views/404', {
+        locals : {
+          request : req,
+          apps    : apps
+        }
+      });
     });
+    
+    rest.use(function(err, req, res) {
+      console.error(err);
+      
+      res.statusCode = 500;
+      res.render(__dirname + '/views/500', {
+        locals : {
+          request : req,
+          apps    : apps,
+          msg     : 'Sorry an error has occurred.'
+        }
+      });
+    });
+    
+    var server = require('http').createServer(rest);
+    pubsub.attach(server);
+    server.listen(rest.get('port'), function() {
+      console.log('listening on port ' + rest.get('port'));
+    });
+  });
+}
+
+function parseArguments(program) {
+  program
+    .version('0.0.1')
+    .usage('[options]')
+    .option('-e, --environment <name>', 'Environment', String, process.env.NODE_ENV)
+    .parse(process.argv);
+
+  if (!program.environment) {
+    console.error('No environment specified.'.red)
+    process.exit(1);
+  }
 }
 
 process.on('uncaughtException', function(err) {
-    console.log(err);
+  console.log(err);
 });
 
 init();
